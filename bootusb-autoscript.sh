45092cd46343f14027b76074084804f0f131eab9
porlar37 <jonatanhakansson@protonmail.com>
2016-04-09 20:53:39 +0200

    Added files via upload

--- bootusb-autoscript.sh
+++ bootusb-autoscript.sh
@@ -2,15 +2,14 @@
 
 cd /home/$USER/Documents
 user=$USER
-drive=read
-echo $user
+read drive
 echo "Downloding linuxmint 17.3 from: http://gensho.acc.umu.se"
 wget http://gensho.acc.umu.se/mirror/linuxmint.com/iso/stable/17.3/linuxmint-17.3-xfce-64bit.iso
 
 echo "Unmounting:"
-sudo umount /dev/sdc
+sudo umount $drive
 
 echo "Writhing the iso"
-sudo dd if=/home/$user/Documents/linuxmint-17.3-3-xfce-64bit.iso of=/dev/sdc
+sudo dd if=/home/$user/Documents/linuxmint-17.3-3-xfce-64bit.iso of=$drive
 
 echo "Done"
