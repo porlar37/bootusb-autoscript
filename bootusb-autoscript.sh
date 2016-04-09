e0273ba0fc1ecaf32c8d449f9f7221f790f1a732
porlar37 <jonatanhakansson@protonmail.com>
2016-04-09 20:56:32 +0200

    Added files via upload

--- bootusb-autoscript.sh
+++ bootusb-autoscript.sh
@@ -13,3 +13,9 @@ echo "Writhing the iso"
 sudo dd if=/home/$user/Documents/linuxmint-17.3-3-xfce-64bit.iso of=$drive
 
 echo "Done"
+
+#/* This program is free software. It comes without any warranty, to
+#     * the extent permitted by applicable law. You can redistribute it
+#     * and/or modify it under the terms of the Do What The Fuck You Want
+#     * To Public License, Version 2, as published by Sam Hocevar. See
+#     * http://www.wtfpl.net/ for more details. */
